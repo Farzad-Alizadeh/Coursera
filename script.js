5b31c865fb1342aeb3e10a8eca2688a303669983
farzada1234 <92983993+farzada1234@users.noreply.github.com>
2021-11-26 01:56:11 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function () {
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Farzad", "Hasan", "Ali", "Hossein", "Armin", "Behnam", "Ahmad", "Parisa", "Mahsa", "Somayeh"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
