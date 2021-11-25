7f4fdea1b381fd3e49fa076e0265f0276c37b40c
farzada1234 <92983993+farzada1234@users.noreply.github.com>
2021-11-26 02:09:48 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// (function () {
-// var names = ["Farzad", "Hasan", "Ali", "Hossein", "Armin", "Behnam", "Ahmad", "Parisa", "Mahsa", "Somayeh"];
-// for (var i = 0; i < names.length; i++) {
-//   var firstLetter = names[i].charAt(0).toLowerCase();
-//   if (firstLetter === 'j') {
-//     byeSpeaker(names[i]);
-//   }
-//   else {
-//     helloSpeaker(names[i]);
-//   }
-// }
-// })();
 (function () {
 
 var names = ["Farzad", "jafar", "jusmin", "justin", "jaber", "Behnam", "Ahmad", "Parisa", "Mahsa", "Somayeh"];
