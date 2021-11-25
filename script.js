526b726cb986d9ef012958599fea5507443298a4
farzada1234 <92983993+farzada1234@users.noreply.github.com>
2021-11-26 02:01:43 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+// (function () {
+// var names = ["Farzad", "Hasan", "Ali", "Hossein", "Armin", "Behnam", "Ahmad", "Parisa", "Mahsa", "Somayeh"];
+// for (var i = 0; i < names.length; i++) {
+//   var firstLetter = names[i].charAt(0).toLowerCase();
+//   if (firstLetter === 'j') {
+//     byeSpeaker(names[i]);
+//   }
+//   else {
+//     helloSpeaker(names[i]);
+//   }
+// }
+// })();
 (function () {
-var names = ["Farzad", "Hasan", "Ali", "Hossein", "Armin", "Behnam", "Ahmad", "Parisa", "Mahsa", "Somayeh"];
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
+
   if (firstLetter === 'j') {
     byeSpeaker(names[i]);
   }
@@ -9,4 +24,5 @@ for (var i = 0; i < names.length; i++) {
     helloSpeaker(names[i]);
   }
 }
+
 })();
